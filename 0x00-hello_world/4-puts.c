0553d0181dd344ffd51f8f2dd0e31a91c62553dd
Yasmingamal <Yasmingamal235@gmail.com>
2023-06-19 16:02:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
+ * main - A program that prints aline with function
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
