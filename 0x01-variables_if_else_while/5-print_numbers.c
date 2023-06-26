c9510ba37680d046f2534bf3380a61cd8f120c98
Yasmingamal <Yasmingamal235@gmail.com>
2023-06-26 06:40:32 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main	- prints numbers from 0 t0 9.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+{
+	printf("%d", i)
+}
+putchar('\n');
+return (0);
+}
